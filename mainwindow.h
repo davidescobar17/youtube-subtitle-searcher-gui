5e023cd4326c43ec3f7e45abd2c293fecc8e7904
davidescobar17 <davidescobar17@outlook.com>
2022-01-05 12:02:56 +1030

    Update mainWindow class interface

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "allmatches.h"
+
 #include <QMainWindow>
+#include <string>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,9 +16,37 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
+    // saves the output for the user, if user specified to save to output file
+    void saveOutputToFile(bool saveToFile);
+
+    // looks through subtitle file to find the target term
+    void parseFile(std::string file, std::string target);
+
+    // passes toFind with subtitile files to parseFile
+    // optionally for files with title containing termInTitle
+    void search(std::string toFind, std::string termInTitle);
+
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
 
 private:
     Ui::MainWindow *ui;
+    allMatches* results = new allMatches();
+    QStringList* gOutput = new QStringList();
+    QVector<std::string> inputIfFilesChecked;
+    QVector<std::string> inputIfFolderChecked;
+    QString subDownloadPath = "input";
+    QStringList fileNames;
+    QString dir;
+
 };
 #endif // MAINWINDOW_H
