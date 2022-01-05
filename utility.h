c1ad02be872fe6693ec2a012fbb81234337bb396
davidescobar17 <davidescobar17@outlook.com>
2022-01-05 11:34:40 +1030

    Add utlity class interface

---
+++ utility.h
@@ -0,0 +1,20 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <string>
+
+// check if line read from the subtitle is the timestamp
+bool isTime(std::string line);
+
+// checks if a line in the subtitles contains the searched term
+bool containsTarget(std::string line, std::string target);
+
+// remove tags inside of the subtitles
+std::string removeTags(std::string s);
+
+// convert string to lowercase
+std::string toLowerString(std::string s);
+
+bool videoTitleContainsTarget(std::string line, std::string target);
+
+#endif // UTILITY_H
