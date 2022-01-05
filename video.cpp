80299d75ab8c167eb3b37e6208ab4be0211bd5b3
davidescobar17 <davidescobar17@outlook.com>
2022-01-05 18:58:07 +1030

    Add video class implementation

---
+++ video.cpp
@@ -0,0 +1,36 @@
+#include "video.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+video::video(std::string n, QVector<videoMatch> v){
+
+    vidName = n;
+    vidMatches = v;
+}
+
+std::string video::getName(){
+
+    return vidName;
+}
+
+int video::getMatchesInVideo(){
+
+    return vidMatches.size();
+}
+
+void video::printVideoMatches(QStringList* gOutput){
+
+    int current = 1;
+    int total = getMatchesInVideo();
+
+    // calls the method that lists out the times and passages
+    for(auto x: vidMatches){
+
+        gOutput->append(QString ("%1/%2 %3").arg(current).arg(total).arg(QString::fromStdString(getName())));
+        x.printPassage(vidName, gOutput);
+        current++;
+    }
+}
