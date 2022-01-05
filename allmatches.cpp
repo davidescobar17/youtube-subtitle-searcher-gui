d28baa07d9331e31097af12d38b905be2783a9ab
davidescobar17 <davidescobar17@outlook.com>
2022-01-05 19:12:58 +1030

    Add allMatches class implementation

---
+++ allmatches.cpp
@@ -0,0 +1,45 @@
+#include "allmatches.h"
+#include "utility.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+allMatches::allMatches(){
+
+    totalMatches = 0;
+    totalVideos = 0;
+}
+
+void allMatches::reset(){
+
+    videos.clear();
+    totalVideos = 0;
+    totalMatches = 0;
+}
+
+void allMatches::addVideo(video v){
+
+    videos.push_back(v);
+    totalVideos++;
+    totalMatches = totalMatches + v.getMatchesInVideo();
+}
+
+void allMatches::listAllResults(QStringList* gOutput){
+
+    for(auto x: videos){
+
+        x.printVideoMatches(gOutput);
+    }
+}
+
+int allMatches::getTotalMatches(){
+
+    return totalMatches;
+}
+
+int allMatches::getTotalVideos(){
+
+    return totalVideos;
+}
